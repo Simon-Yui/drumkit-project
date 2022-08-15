199d61f8b56d7c4b9bff831bde384a79756ea053
Lucas Simon <lucasimonmendes@hotmail.com>
2022-08-15 18:04:30 -0300

    adicionando funcionalidade composition

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+// EVENTOS
 document.body.addEventListener('keyup', (event)=> {
     playSound(event.code.toLowerCase() )
 })
 
+document.querySelector('.composer button').addEventListener('click', ()=>{
+    let song = document.querySelector('#input').value
+
+    if(song !== ''){
+        let songArray = song.split('')
+        playComposition(songArray)   
+    }
+})
+
+// FUNÇÕES
 function playSound(sound){
     let audioElement = document.querySelector(`#s_${sound}`)
     let keyElement = document.querySelector(`div[data-key="${sound}"]`)
 
     if(audioElement) {
-        audioElement.currentTime = 0;
-        audioElement.play();
+        audioElement.currentTime = 0
+        audioElement.play()
     }
 
     if(keyElement) {
@@ -17,5 +28,18 @@ function playSound(sound){
        setTimeout(()=>{
         keyElement.classList.remove('active')
        }, 300)
+    }
+}
+
+function playComposition(songArray){
+    let wait = 0
+
+    for(let songItem of songArray){
+        setTimeout(()=>{
+            playSound(`key${songItem}`)
+
+        }, wait)
+
+        wait +=250
     }
 }
